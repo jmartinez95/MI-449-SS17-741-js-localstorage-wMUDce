4a89df734f2c91235493b736a538d27fc4fe0d83
jmartinez95 <mart1527@msu.edu>
2017-02-21 13:04:44 -0500

    Fixed 2 Spaces

--- changeTheme.js
+++ changeTheme.js
@@ -1,11 +1,11 @@
 var visits = localStorage.getItem("visits");
 if(visits == null) {
-	visits = 1;
-	localStorage.setItem("visits", visits);
+  visits = 1;
+  localStorage.setItem("visits", visits);
 } else {
-	visits = parseInt(visits);
-	visits += 1;
-	localStorage.setItem("visits", visits);
+  visits = parseInt(visits);
+  visits += 1;
+  localStorage.setItem("visits", visits);
 }
 
 document.getElementById('visits').innerHTML = visits;
@@ -15,19 +15,19 @@ changeTheme("false")
 var buttonElement = document.getElementById('change_theme');
 
 buttonElement.addEventListener('click', function () {
-	changeTheme("true");
+  changeTheme("true");
 });
 
 function changeTheme(update) {
   var theme = localStorage.getItem("theme");
-	if(theme == null || (theme == "night" && update == "false") || (theme == "day" && update == "true")) {
-		theme = "night";
-		document.body.setAttribute('class', 'night-theme');
-	} else {
-		theme = "day";
-		document.body.setAttribute('class', 'day-theme');
-	}
-	if(update == "true") {
-		localStorage.setItem("theme", theme);
-	}
+  if(theme == null || (theme == "night" && update == "false") || (theme == "day" && update == "true")) {
+    theme = "night";
+    document.body.setAttribute('class', 'night-theme');
+  } else {
+    theme = "day";
+    document.body.setAttribute('class', 'day-theme');
+  }
+  if(update == "true") {
+    localStorage.setItem("theme", theme);
+  }
 }
